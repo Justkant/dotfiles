fca9dc059fec6f9623e1906ca868e2c45140ec03
Kant <quent92100@gmail.com>
2021-12-23 08:54:48 +0100

    fix: n-install script

--- run_once_after_80-install-n-linux.sh
+++ run_once_after_80-install-n-linux.sh
@@ -2,6 +2,9 @@
 
 if [ ! -d "${HOME}/n" ] ; then
   curl -L https://git.io/n-install | bash
+
+  export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+
   n latest
   corepack enable
 fi
